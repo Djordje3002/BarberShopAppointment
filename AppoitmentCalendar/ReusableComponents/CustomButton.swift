0fe3e318517bd74c7c9e109dc1621e68eb7148b5
Djordje3002 <djkartaljevic@gmail.com>
2025-07-29 22:00:27 +0200

    A lot of code. HomeView UI and ContentView with map and logic

---
+++ AppoitmentCalendar/ReusableComponents/CustomButton.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButton.swift
+//  AppoitmentCalendar
+//
+//  Created by Djordje on 29. 7. 2025..
+//
+
+import SwiftUI
+
+struct CustomButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButton()
+}
